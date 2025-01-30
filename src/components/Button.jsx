558ef208630f75125385123cf9ff284510e2d066
GagenvirGill <gagenvirg@gmail.com>
2025-01-30 15:12:25 -0800

    Updated the closet page UI

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import "../styles/Button.css"
 
-const Button = ({ type, text, disable }) => {
+const Button = ({ type, text, disable, id }) => {
     return (
         <button className="custom-button" disabled={disable} type={type}>{text}</button>
     );
--- src/pages/Closet.jsx
+++ src/pages/Closet.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-import CategoriesButtonList from "../components/CategoriesButtonList";
 import FilterItemsForm from "../components/FilterItemsForm";
-import AddCategoryForm from "../components/AddCategoryForm";
-import AddItemForm from "../components/AddItemForm";
 import AllItemDisplay from "../components/AllItemDisplay";
 
 import "../styles/Closet.css";
@@ -11,15 +8,13 @@ import "../styles/Closet.css";
 const Closet = () => {
     return (
         <div className="closetPage">
-            <AddCategoryForm />
+            <FilterItemsForm />
             <br />
-            <AddItemForm />
+            <br />
+            <br />
+            <br />
             <br />
             <AllItemDisplay />
-            <br />
-            <br />
-            <br />
-            <FilterItemsForm />
         </div>
     );
 };
