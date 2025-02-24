83bc932173ec70cf8669831fc02be55f94040c93
GagenvirGill <gagenvirg@gmail.com>
2025-02-23 23:20:42 -0800

    Using the image-js library, added cropping for removing columns/rows that are transparent

--- src/api/Img.js
+++ src/api/Img.js
@@ -1,12 +1,32 @@
 import { removeBackground } from "@imgly/background-removal";
+import Image from "image-js";
 
 export const imgBackgroundRemoval = async (imgPath) => {
 	try {
-		const blob = await removeBackground(imgPath);
-		const file = new File([blob], "img.jpg", { type: "image/jpeg" });
+		const blob = await removeBackground(imgPath, {
+			output: {
+				format: "image/png",
+			},
+		});
+		const croppedBuffer = await removeTransparentEdges(blob);
+		const file = new File([croppedBuffer], "img.png", {
+			type: "image/png",
+		});
 		return file;
 	} catch (err) {
 		console.log(`Error removing background: ${err}`);
 		throw err;
 	}
+};
+
+const removeTransparentEdges = async (blob) => {
+	const arrayBuffer = await blob.arrayBuffer();
+	const img = await Image.load(arrayBuffer);
+
+	const croppedImg = img.cropAlpha({
+		threshold: 50,
+	});
+
+	const croppedBuffer = croppedImg.toBuffer();
+	return croppedBuffer;
 };
