077a25d3290a5631d1857ed1af3cc9ef12989ebc
GagenvirGill <gagenvirg@gmail.com>
2025-03-31 07:18:59 -0700

    Updated a Radio Buttons Styling

--- src/components/buttons/RadioButton.jsx
+++ src/components/buttons/RadioButton.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import styles from "./RadioButton.module.css";
 
 const RadioButton = ({ text, buttonId, value, checked, onChange }) => {
-    return (
-        <>
-            <label className={styles.radioButton} htmlFor={buttonId}>
-                <input
-                    type="radio"
-                    id={buttonId}
-                    name="navDropdownOptions"
-                    value={value}
-                    checked={checked}
-                    onChange={onChange}
-                />
-                {text}
-            </label>
-        </>
-    );
+	return (
+		<>
+			<label className={styles.radioButton} htmlFor={buttonId}>
+				<input
+					type="radio"
+					id={buttonId}
+					name={buttonId}
+					value={value}
+					checked={checked}
+					onChange={onChange}
+				/>
+				{text}
+			</label>
+		</>
+	);
 };
 
 export default RadioButton;
