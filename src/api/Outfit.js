2ba743b083fe4c1f4af1609d749fd61fd27f310f
GagenvirGill <gagenvirg@gmail.com>
2025-04-02 13:58:16 -0700

    Implemented methods to call the backend API endpoints for an outfit

---
+++ src/api/Outfit.js
@@ -0,0 +1,88 @@
+import ax from "./axiosConfig";
+
+export const getAllOutfits = async () => {
+	try {
+		const response = await ax.get("/outfit");
+		if (response.data.success === true) {
+			console.log(response.data.message);
+			return response.data.data;
+		} else {
+			console.error(response.data.message);
+			alert(response.data.message);
+		}
+	} catch (err) {
+		console.error(err);
+		alert("Something went wrong when getting outfits");
+	}
+};
+
+export const createOutfit = async (dateWorn, description, items) => {
+	try {
+		const response = await ax.post("/outfit", {
+			dateWorn: dateWorn,
+			description: description,
+			items: items,
+		});
+		if (response.data.success === true) {
+			console.log(response.data.message);
+			return response.data.data;
+		} else {
+			console.error(response.data.message);
+			alert(response.data.message);
+		}
+	} catch (err) {
+		console.error(err);
+		alert("Something went wrong when creating an outfit");
+	}
+};
+
+export const deleteOutfit = async (outfitId) => {
+	try {
+		const response = await ax.delete(`/outfit/${outfitId}`);
+		if (response.data.success === true) {
+			console.log(response.data.message);
+		} else {
+			console.error(response.data.message);
+			alert(response.data.message);
+		}
+	} catch (err) {
+		console.error(err);
+		alert("Something went wrong when deleting an outfit");
+	}
+};
+
+export const addItemsToOutfit = async (outfitId, items) => {
+	try {
+		const response = await ax.post(`/outfit/${outfitId}/items`, {
+			items: items,
+		});
+
+		if (response.data.success === true) {
+			console.log(response.data.message);
+		} else {
+			console.error(response.data.message);
+			alert(response.data.message);
+		}
+	} catch (err) {
+		console.error(err);
+		alert("Something went wrong when adding items to an outfit");
+	}
+};
+
+export const removeItemsFromOutfit = async (outfitId, items) => {
+	try {
+		const response = await ax.delete(`/outfit/${outfitId}/items`, {
+			data: { items: items },
+		});
+
+		if (response.data.success === true) {
+			console.log(response.data.message);
+		} else {
+			console.error(response.data.message);
+			alert(response.data.message);
+		}
+	} catch (err) {
+		console.error(err);
+		alert("Something went wrong when removing items from an outfit");
+	}
+};
