2e29212440665bf91653641895ab2189fd3ce80a
GagenvirGill <gagenvirg@gmail.com>
2025-04-11 15:15:26 -0700

    Updated create outfit api method to work within the new outfit template system

--- src/api/Outfit.js
+++ src/api/Outfit.js
@@ -17,49 +17,21 @@ export const getAllOutfits = async () => {
 	}
 };
 
-export const getItemsForAnOutfit = async (outfitId) => {
+export const createOutfit = async (dateWorn, description, itemsMap) => {
 	try {
-		const response = await ax.get(`/outfit/${outfitId}/items`);
-		if (response.data.success === true) {
-			console.log(response.data.message);
-			return response.data.data;
-		} else {
-			console.error(response.data.message);
-			alert(response.data.message);
-		}
-	} catch (err) {
-		console.error(err);
-		alert("Something went wrong when getting an outfits items");
-	}
-};
-
-export const createOutfit = async (dateWorn, description, items, imageFile) => {
-	try {
-		let processedImage = null;
-
-		if (imageFile) {
-			const { success, data, message } = await workerPool.processImage(
-				imageFile
-			);
-			if (!success) {
-				throw new Error(message);
-			}
-			processedImage = data;
-		}
-
-		const formData = new FormData();
-		if (processedImage) {
-			formData.append("image", processedImage);
-		}
-		formData.append("dateWorn", dateWorn);
-		formData.append("description", description);
-		formData.append("items", items);
-
-		const response = await ax.post("/outfit", formData, {
-			headers: {
-				"Content-Type": "multipart/form-data",
+		const response = await ax.post(
+			"/outfit",
+			{
+				dateWorn: dateWorn,
+				description: description,
+				items: itemsMap,
 			},
-		});
+			{
+				headers: {
+					"Content-Type": "multipart/form-data",
+				},
+			}
+		);
 		if (response.data.success === true) {
 			console.log(response.data.message);
 			return response.data.data;
