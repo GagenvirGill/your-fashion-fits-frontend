cc26fffc4f84cad2984dba72ee32561c77f2753e
GagenvirGill <gagenvirg@gmail.com>
2025-04-28 22:01:54 -0700

    For some reason an old file wasn't commited

---
+++ src/components/popupForms/categoryContextMenu/AddCategoryToItemsForm.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import styles from "../ContextMenuPopUpStyles.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { refreshState } from "../../../store/reducers/categoriesReducer";
+
+import { addCategoryToItems } from "../../../api/Category";
+
+import ItemsCheckboxForm from "../../forms/ItemsCheckboxForm";
+
+const AddCategoryToItemsForm = ({
+	categoryId,
+	handleClose,
+	categoriesCurrItems,
+}) => {
+	const dispatch = useDispatch();
+	const { items } = useSelector((state) => state.items);
+	const [filteredItems, setFilteredItems] = useState([]);
+
+	useEffect(() => {
+		const currItems = new Set();
+		categoriesCurrItems.map((item) => {
+			currItems.add(item.itemId);
+		});
+
+		const filtItems = items.filter((item) => {
+			return !currItems.has(item.itemId);
+		});
+
+		setFilteredItems(filtItems);
+	}, [items, categoriesCurrItems]);
+
+	const handleSubmit = async (selectedItems) => {
+		await addCategoryToItems(categoryId, selectedItems);
+		dispatch(refreshState());
+		handleClose();
+	};
+
+	return (
+		<div>
+			<p className={styles.formTitle}>
+				Select Which Items to Add to this Category
+			</p>
+			<ItemsCheckboxForm
+				handleSubmit={handleSubmit}
+				displayItems={filteredItems}
+			/>
+		</div>
+	);
+};
+
+export default AddCategoryToItemsForm;
