639b8b99a4143a7ddbbfbaa6a57e2ad5c4649d3b
GagenvirGill <gagenvirg@gmail.com>
2025-05-05 21:09:43 -0700

    Typo

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
-import ProfilePopup from "./profilePopup/profilePopup";
+import ProfilePopup from "./profilePopup/ProfilePopup";
 
 const Navbar = ({ setIsAuthenticated }) => {
 	return (
