4290bff59b04e496f5e205d65e736b2db3202236
GagenvirGill <gagenvirg@gmail.com>
2025-05-13 17:52:19 -0700

    Implemented item display load more pagination as well

--- src/components/cardDisplay/ItemCardDisplay.jsx
+++ src/components/cardDisplay/ItemCardDisplay.jsx
@@ -4,31 +4,43 @@ import styles from "./CardDisplayStyles.module.css";
 
 import { filterItemsByCategories } from "../../api/Item";
 
+import Button from "../buttons/Button";
 import ItemCard from "../card/ItemCard";
 
 const ItemCardDisplay = ({ selectedCategories }) => {
+	const LOAD_MORE_AMOUNT = 32;
 	const dispatch = useDispatch();
+
 	const { items } = useSelector((state) => state.items);
+
 	const [displayItems, setDisplayItems] = useState([]);
+	const [visibleCount, setVisibleCount] = useState(LOAD_MORE_AMOUNT);
+
+	const handleLoadMore = () => {
+		setVisibleCount((prev) => prev + LOAD_MORE_AMOUNT);
+	};
 
 	useEffect(() => {
 		if (selectedCategories && selectedCategories.length > 0) {
 			filterItemsByCategories(selectedCategories)
 				.then((fetchedItems) => {
+					setVisibleCount(LOAD_MORE_AMOUNT);
 					setDisplayItems(fetchedItems);
 				})
 				.catch((err) => {
 					console.log(`Error loading items: ${err}`);
 				});
 		} else {
+			setVisibleCount(LOAD_MORE_AMOUNT);
 			setDisplayItems(items);
 		}
 	}, [dispatch, selectedCategories, items]);
 
 	return (
 		<>
+			<br />
 			<div className={styles.cardDisplay}>
-				{displayItems.map((item) => (
+				{displayItems.slice(0, visibleCount).map((item) => (
 					<ItemCard
 						key={`${item.itemId}-${selectedCategories}`}
 						itemId={item.itemId}
@@ -36,6 +48,14 @@ const ItemCardDisplay = ({ selectedCategories }) => {
 					/>
 				))}
 			</div>
+			<br />
+			{visibleCount < displayItems.length && (
+				<Button
+					type="submit"
+					text="Load More"
+					onClick={handleLoadMore}
+				/>
+			)}
 		</>
 	);
 };
